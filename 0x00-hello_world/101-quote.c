5b0f907b7f500d2c49a6177c6565c281f031d82e
Zatimila <timmydakolo17@gmail.com>
2023-08-13 12:43:08 -0700

    complicated sh*t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- 	* main - this prints exactly the input on it
- 	* Return: always 1.
-	*/
+ * main - main function
+ *
+ * Return: 1
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
